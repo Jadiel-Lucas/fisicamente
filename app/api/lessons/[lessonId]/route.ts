f830515b0de2518fe5c0585f86658c21f6928088
Jadiel-Lucas <jadiellucas00@gmail.com>
2025-09-12 01:04:05 -0300

    sei lá, agora é no lessons 41:29

---
+++ app/api/lessons/[lessonId]/route.ts
@@ -0,0 +1,40 @@
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+import db from "@/db/drizzle";
+import { lessons } from "@/db/schema";
+import { getIsAdmin } from "@/lib/admin";
+
+export const GET = async (
+    req: Request,
+    { params }: { params: { lessonId: number } },
+) => {
+
+    const isAdmin = await getIsAdmin();
+
+    if (!getIsAdmin()) {
+        return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const body = await req.json();
+    const data = await db.update(lessons).set({
+        ...body,
+    }).where(eq(lessons.id, params.lessonId)).returning();
+
+    return NextResponse.json(data[0]);
+};
+
+
+export const DELETE = async (
+    req: Request,
+    { params }: { params: { lessonId: number } },
+) => {
+    if (!getIsAdmin()) {
+        return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const data = await db.delete(lessons)
+        .where(eq(lessons.id, params.lessonId)).returning();
+
+    return NextResponse.json(data[0]); //diz que precisa de data[0]
+};
