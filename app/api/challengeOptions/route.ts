06db7494f448816da4b58e89250459f353fc50ec
Jadiel-Lucas <jadiellucas00@gmail.com>
2025-09-14 02:48:37 -0300

    parte final

--- app/api/challengeOptions/route.ts
+++ app/api/challengeOptions/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import db from "@/db/drizzle";
 import { getIsAdmin } from "@/lib/admin";
-import { challenges } from "@/db/schema";
+import { challengeOptions } from "@/db/schema";
 
 export const GET = async () => {
     const isAdmin = await getIsAdmin(); // diz que precisa de await
@@ -11,7 +11,7 @@ export const GET = async () => {
         return new NextResponse("Unauth", { status: 401 });
     }
     
-    const data = await db.query.challenges.findMany();
+    const data = await db.query.challengeOptions.findMany();
 
     return NextResponse.json(data);
 };
@@ -25,7 +25,7 @@ export const POST = async (req: Request) => {
 
     const body = await req.json();
     
-    const data = await db.insert(challenges).values({
+    const data = await db.insert(challengeOptions).values({
         ...body,
     }).returning();
 
