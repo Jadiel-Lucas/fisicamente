4195a306bc8c0240c98c680e4b2830a74b925816
Jadiel-Lucas <jadiellucas00@gmail.com>
2025-09-16 20:31:36 -0300

    initial commit

--- app/lesson/[lessonId]/page.tsx
+++ app/lesson/[lessonId]/page.tsx
@@ -1,46 +1,36 @@
 import { redirect } from "next/navigation";
 
 import { getLesson, getUserProgress } from "@/db/queries";
-
 import { Quiz } from "../quiz";
 
-type Props = {
-    params: {
-        lessonId: number;
-    }
+export default async function LessonIdPage(
+  props: { params: Promise<{ lessonId: string }> }
+) {
+  const { lessonId } = await props.params; // <- await aqui
+
+  const lessonIdNumber = Number(lessonId);
+
+  const [lesson, userProgress] = await Promise.all([
+    getLesson(lessonIdNumber),
+    getUserProgress(),
+  ]);
+
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
+
+  const initialPercentage =
+    (lesson.challenges.filter((c) => c.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      // userSubscription={null}
+    />
+  );
 }
-
-const LessonIdPage = async ({
-    params,
-}: Props) => {
-    const lessonData = getLesson(params.lessonId);
-    const userProgressData = getUserProgress();
-
-    const [
-        lesson,
-        userProgress,
-    ] = await Promise.all([
-        lessonData,
-        userProgressData
-    ]);
-
-    if (!lesson || !userProgress) {
-        redirect("/learn");
-    }
-
-    const initialPercentage = lesson.challenges
-        .filter((challenge) => challenge.completed)
-        .length / lesson.challenges.length * 100;
-
-    return (
-        <Quiz 
-            initialLessonId={lesson.id}
-            initialLessonChallenges={lesson.challenges}
-            initialHearts={userProgress.hearts}
-            initialPercentage={initialPercentage}
-            //userSubscription={null} //add user subscription
-        />
-    );
-};
-
-export default LessonIdPage;
